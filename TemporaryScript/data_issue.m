dc33ea31d039bfc079286f7262b0e6588353e3ea
wangxunjue <hzwangxunjue@corp.netease.com>
2015-07-23 19:33:53 +0800

    0723-733

--- TemporaryScript/data_issue.m
+++ TemporaryScript/data_issue.m
@@ -1,15 +1,16 @@
 clear
-fd = fopen( 'C:\memory.bin','r' );
+close
+fd = fopen( 'C:\memory.bin', 'r' );
 data = fread( fd, 'int16' );
 subplot( 211 );
 plot( data );
 axis( [0,3200,-32768,32768] );
-xlabel( 'without hp filter' )
+title( 'with no HP filter' )
 %now create a hp filter
-num = [.92724603 -1.8544941 0.927246903];
-den = [1 1.906005859 0.911376953];
-y=filter( num, den, data );
+num = [0.927246093 -1.8544941 0.927246903];
+den = [1 -1.906005859 0.911376953];
+y = filter( num, den, data );
 subplot(212);
-plot( data );
+plot( y );
 axis( [0,3200,-32768,32768] );
-xlabel( 'with hp filter' )
+title( 'with HP filter' )
